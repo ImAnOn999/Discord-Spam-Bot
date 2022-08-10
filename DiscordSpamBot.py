8b93e9c0fe14632d7fe8027770f7c3079b810987
ImAnOn999 <107021489+ImAnOn999@users.noreply.github.com>
2022-08-10 14:50:12 -0700

    Update DiscordSpamBot.py

--- DiscordSpamBot.py
+++ DiscordSpamBot.py
@@ -1,10 +1,8 @@
 import requests
 import time
 
-authorizationCodes = ["MTAwNjM0Mzc3Mjg1NjEzNTgyMg.GiDOnk.SDu7DJ0flOLKxCrfrtdo-4Z3pf_BXgPIZmn1tY","OTc5Mzk4NDIwNTYzMzY1OTkw.G96Yrp.IHHTdRy8aSCPpYvA6FPlc4_PnpemFiHP2hZr-M"]#Add authorization codes for accounts you want to use here
-spamMessages = ["test","test2"]#Add messages you want to send here
-
-messagesSent = 0
+authorizationCodes = [""]#Add authorization codes for accounts you want to use here
+spamMessages = [""]#Add messages you want to send here
 
 id = input("Insert id of channel or user you want to spam: ")
 timer = int(input("Insert time until attack (In Seconds): "))
